2d17b147c4b7ffb9e9c1257b05efe2b746027807
sarahsakordaniels <ssdaniels13@gmail.com>
2019-06-13 10:55:14 -0600

    working auth, trying to create user contorller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,9 +3,9 @@ class AuthenticationController < ApplicationController
 
   def authenticate
     command = AuthenticateUser.call(params[:email], params[:password])
-
+    current_user= User.find_by(email: params[:email])
     if command.success?
-      render json: { auth_token: command.result }
+      render json: { auth_token: command.result, user_id: current_user.id}
     else
       render json: { error: command.errors }, status: :unauthorized
     end
